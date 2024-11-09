6c6da7b190bd85d4fa0dc40b77f9c0f2bb288908
error-v0e <Pej2202@seznam.cz>
2024-11-09 16:07:38 +0100

    Add Sequelize and SQLite3 dependencies; remove unused API call in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,6 @@ import axios from 'axios';
 import {Calendar} from "@nextui-org/calendar";
 import {parseDate} from '@internationalized/date';
 
-const apiCall = () => {
-  axios.get('http://localhost:8080').then((data) => {
-    console.log(data)
-  })
-}
 
 function App() {
 
@@ -15,7 +10,6 @@ function App() {
     <div className="flex gap-x-4">
       <Calendar aria-label="Date (No Selection)" />
       <Calendar aria-label="Date (Uncontrolled)" defaultValue={parseDate("2020-02-03")} />
-      <button onClick={apiCall}>Make API Call</button>
     </div>
     </>
   )
