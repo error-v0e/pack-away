18ee464c66571c3771d355cb41c89882acfc10b2
error-v0e <Pej2202@seznam.cz>
2025-01-19 16:48:25 +0100

    Remove unnecessary comment in Vite configuration and maintain host setting for server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
     server: {
@@ -13,6 +12,6 @@ export default defineConfig({
         }
       },
       historyApiFallback: true,
-      host: true, // nebo '0.0.0.0'
+      host: true, 
     },
 })
