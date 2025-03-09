63ac633ed7dacd4f19416745e0f30ff3a560f260
error-v0e <Pej2202@seznam.cz>
2025-03-09 01:29:21 +0100

    Add tooltip to display list ID in List component

--- frontend/src/pages/List.jsx
+++ frontend/src/pages/List.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Accordion, AccordionItem, Card, CardHeader, CardBody, Input, Autocomplete, AutocompleteItem, AutocompleteSection, Button, Popover, PopoverTrigger, PopoverContent, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure } from "@heroui/react";
+import { Accordion, AccordionItem, Card, CardHeader, CardBody, Input, Autocomplete, AutocompleteItem, AutocompleteSection, Button, Popover, PopoverTrigger, PopoverContent, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure, Tooltip } from "@heroui/react";
 import { Flex } from 'antd';
 import { useNavigate } from 'react-router-dom';
 
@@ -32,17 +32,20 @@ const List = () => {
         <Button onClick={handleButtonClick} size="lg" className='ps-4 pe-4 min-h-[60px]'>
           Nový seznam
         </Button>
+        
       </Flex>
       <Flex wrap gap="small" justify="center" className="mb-5">
         {lists.map(list => (
-          <Card key={list.id_list} className="max-w-[240px] w-full">
-            <CardHeader className="flex gap-3">
-              <div className="flex flex-col">
-                <p className="text-xl">{list.name}</p>
-                <p className="text-small text-default-500">Počet položek: {list.itemCount}</p>
-              </div>
-            </CardHeader>
-          </Card>
+          <Tooltip content={"#"+list.id_list}>
+            <Card key={list.id_list} className="max-w-[240px] w-full">
+              <CardHeader className="flex gap-3">
+                <div className="flex flex-col">
+                  <p className="text-xl">{list.name}</p>
+                  <p className="text-small text-default-500">Počet položek: {list.itemCount}</p>
+                </div>
+              </CardHeader>
+            </Card>
+          </Tooltip>
         ))}
       </Flex>
     </>
